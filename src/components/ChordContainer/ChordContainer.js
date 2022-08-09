9070f845cf1dbfe0ea0ee8e69df45a3d564e7c0e
mattnoth <musicnoth@gmail.com>
2022-08-09 00:08:27 -0500

    different drag slider attempts switching to draggable

--- src/components/ChordContainer/ChordContainer.js
+++ src/components/ChordContainer/ChordContainer.js
@@ -1,22 +1,15 @@
 import React from 'react';
+// import RangeSlider from './RangeSlider.js';
+import DragSlider from './DragSlider.js';
 import './styles.css'
 
 function ChordContainer({ line }) {
 
-    let rangeslider = document.getElementById("sliderRange");
-    let output = document.getElementById("demo");
-    output.innerHTML = rangeslider.value;
-
-    rangeslider.oninput = function () {
-        output.innerHTML = this.value;
-    }
     return (
         <div className="chordContainer">
             this is chord container {line?.currentId}
-            <div class="rangeslider">
-                <input type="range" min="1" max="100" value="10"
-                    class="myslider" id="sliderRange"> </input>
-            </div>
+            {/* <RangeSlider /> */}
+            <DragSlider />
         </div>
     );
 }
---
+++ src/components/ChordContainer/DragSlider.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DragSlider(props) {
+    return (
+        <>
+            <canvas className="canvasEl">
+            </canvas>
+        </>
+    );
+}
+
+export default DragSlider;
