9cad444009380246e688efac7f08524a4bc00a53
mattnoth <musicnoth@gmail.com>
2022-08-30 09:06:57 -0500

    chore: simple commit for remote transfer

--- src/components/ChordContainer/ChordContainer.js
+++ src/components/ChordContainer/ChordContainer.js
@@ -7,9 +7,9 @@ function ChordContainer({ line }) {
 
     // plus button is a new Draggable element 
 
-    const [dragContent, setDragContent] = useState('')
+    // const [dragContent, setDragContent] = useState('')
 
-    const el = document.createElement("div");
+    // const el = document.createElement("div");
 
     // class Chord {
     //     constructor(height, width) {
@@ -17,27 +17,52 @@ function ChordContainer({ line }) {
     //     }
     // }
 
-    const onClick = () => {
-        console.log('clikced');
+    // const onClick = () => {
+    //     console.log('clikced');
 
 
 
-        const drag = <Draggable><p>${dragContent}</p></Draggable>
-        el.innerHTML(drag)
-        console.log(el)
+    //     const drag = <Draggable><p>${dragContent}</p></Draggable>
+    //     el.innerHTML(drag)
+    //     console.log(el)
 
-        setDragContent(drag)
+    //     setDragContent(drag)
+    // }
+
+
+    // its because its an array you dodo bird 
+    const chords = [{
+        id: 1,
+        chordToDisplay: "C"
+    },
+    {
+        id: 2,
+        chordToDisplay: "F"
+    },
+    {
+        id: 3,
+        chordToDisplay: "G"
     }
+    ];
 
+
+    console.log(chords.chordToDisplay)
 
 
 
     return (
         <div className="chordContainer">
-            <button onClick={onClick}>Click me</button>
-            <Draggable>
-                <p>Ab</p>
-            </Draggable>
+
+            {chords.map((...chord) => (
+                <div className="chordToDisplay">
+                    <Draggable>
+                        <p>
+                            {chord.chordToDisplay}
+                        </p>
+                    </Draggable>
+                </div>
+            ))}
+            Chord Container
         </div>
     );
 }
